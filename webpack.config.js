6ebfe4e07f6611a6718e20d1bf339cf9390640bb
Kimberley Kelly <kimberley.b.kelly@gmail.com>
2022-03-02 16:09:27 +0000

    update with additional packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,50 @@
+require('dotenv').config()
+
+const { DefinePlugin } = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // Where files should be sent once they are bundled
- output: {
-   path: path.join(__dirname, '/dist'),
-   filename: 'index.bundle.js'
- },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
   // webpack 5 comes with devServer which loads in development mode
- devServer: {
-   port: 3000,
-   hot: true,
-   open: true
- },
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+  },
   // Rules of how webpack will take our files, complie & bundle them for the browser
- module: {
-   rules: [
-     {
-       test: /\.(js|jsx)$/,
-       exclude: /nodeModules/,
-       use: {
-         loader: 'babel-loader'
-       }
-     },
-     {
-       test: /\.css$/,
-       use: ['style-loader', 'css-loader']
-     }
-   ]
- },
- plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env': {},
+    }),
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+  ],
 }
